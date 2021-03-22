7fe1a98329a49d345a5dc6894eb1bc47a201175b
shashi.kumar@microchip.com <shashi.kumar@microchip.com>
2021-03-22 17:32:28 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=sample.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/sample.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=sample.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sample.tar
